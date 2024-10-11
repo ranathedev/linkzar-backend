1e0d83a3ddc512e859d199d650714e0b05219b25
ranaintizar <intizaralirana2@gmail.com>
2024-10-11 11:44:09 +0500

    Deploy on vercel

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const { MongoClient } = require('mongodb')
-const fs = require('fs')
-require('dotenv').config()
+const { MongoClient } = require("mongodb")
+const fs = require("fs")
+require("dotenv").config()
 const {
   createCollection,
   deleteCollection,
@@ -10,85 +10,85 @@ const {
   deleteLink,
   deleteDemoLinks,
   editLink,
-} = require('./module.js')
+} = require("./module.js")
 
-const fastify = require('fastify')({
+const fastify = require("fastify")({
   logger: false,
 })
 
-fastify.register(require('@fastify/cors'), {})
+fastify.register(require("@fastify/cors"), {})
 
 const uri = process.env.MONGO_URI
 const client = new MongoClient(uri)
 
-fastify.get('/', async (req, res) => {
+fastify.get("/", async (req, res) => {
   try {
     const htmlContent = await fs.promises.readFile(
-      './public/index.html',
-      'utf8'
+      "./public/index.html",
+      "utf8"
     )
-    res.header('Content-Type', 'text/html')
+    res.header("Content-Type", "text/html")
     res.send(htmlContent)
   } catch (err) {
     console.error(err)
-    res.status(500).send('Internal Server Error')
+    res.status(500).send("Internal Server Error")
   }
 })
 
-fastify.post('/api/getLinks', async (req, res) => {
+fastify.post("/api/getLinks", async (req, res) => {
   const uid = req.body.uid
   const response = await getLinks(client, uid)
   res.send(response)
 })
 
-fastify.post('/api/createColl', async (req, res) => {
+fastify.post("/api/createColl", async (req, res) => {
   const uid = req.body.uid
   await createCollection(client, uid, res)
 })
 
-fastify.post('/api/deleteColl', async (req, res) => {
+fastify.post("/api/deleteColl", async (req, res) => {
   const uid = req.body.uid
   await deleteCollection(client, uid, res)
 })
 
-fastify.post('/api/demoLinks', async (req, res) => {
+fastify.post("/api/demoLinks", async (req, res) => {
   const { uid, demoLinks } = req.body
   const response = await insertDocuments(client, uid, demoLinks)
-  if (response === 'Demo Links added to Database')
+  if (response === "Demo Links added to Database")
     res.send(response).status(200)
   else res.send(response).status(500)
 })
 
-fastify.post('/api/delDemoLinksFromMain', async (req, res) => {
+fastify.post("/api/delDemoLinksFromMain", async (req, res) => {
   const demoLinks = req.body
-  console.log(demoLinks, 'demoLinks to delete from Main Collection')
+  console.log(demoLinks, "demoLinks to delete from Main Collection")
   await deleteDemoLinks(client, demoLinks, res)
 })
 
-fastify.post('/api/shorten', async (req, res) => {
+fastify.post("/api/shorten", async (req, res) => {
   const { uid, url, shortId } = req.body
   const dataObject = { shortId, originalURL: url }
   const response = await insertDataObject(client, dataObject, uid)
-  response ? res.send(response) : res.send({ err: 'Unexpected error occured' })
+  response ? res.send(response) : res.send({ err: "Unexpected error occured" })
 })
 
-fastify.post('/api/deleteLink', async (req, res) => {
+fastify.post("/api/deleteLink", async (req, res) => {
   const { uid, id } = req.body
   const response = await deleteLink(client, id, uid)
   res.send(response)
 })
 
-fastify.post('/api/editLink', async (req, res) => {
+fastify.post("/api/editLink", async (req, res) => {
   const { uid, id, value } = req.body
   const response = await editLink(client, id, value, uid)
   res.send(response)
 })
 
-fastify.get('/:shortId', async (req, res) => {
+fastify.get("/:shortId", async (req, res) => {
   const shortId = req.params.shortId
   try {
     await client.connect()
-    const database = client.db('linkzar')
+    const database = client.db("linkzar")
     const userCollections = database.listCollections()
     while (await userCollections.hasNext()) {
       const collectionInfo = await userCollections.next()
@@ -104,16 +104,48 @@ fastify.get('/:shortId', async (req, res) => {
         return
       }
     }
-    res.status(404).send('URL Data not found')
+    res.status(404).send("URL Data not found")
   } catch (error) {
-    console.error('Error retrieving URL:', error)
-    res.status(500).send('Internal Server Error')
+    console.error("Error retrieving URL:", error)
+    res.status(500).send("Internal Server Error")
   } finally {
     await client.close()
   }
 })
 
-fastify.listen({ port: 3000, host: '0.0.0.0' }, function (err, address) {
+// fastify.get('/api/findLink', async (req, res) => {
+//   const { id } = req.query
+
+//   try {
+//     await client.connect()
+//     const database = client.db('linkzar')
+
+//     const userCollections = await database.listCollections()
+
+//     while (await userCollections.hasNext()) {
+//       const collectionInfo = await userCollections.next()
+//       const userCollection = database.collection(collectionInfo.name)
+
+//       const shortLink = await userCollection.findOne({
+//         shortId: id,
+//       })
+
+//       if (shortLink) {
+//         console.log({ collection: collectionInfo.name, data: shortLink })
+//       }
+//     }
+
+//     res.send('OK').status(200)
+//   } catch (error) {
+//     console.error('Error creating user collection:', error)
+//     res.send('Error').status(500)
+//   } finally {
+//     await client.close()
+//   }
+//   res.send(response)
+// })
+
+fastify.listen({ port: 3000, host: "0.0.0.0" }, function (err, address) {
   if (err) {
     console.error(err)
     process.exit(1)
